e7b66acc10012c8b1874afdd4bde284514263292
tchaves <thiagoschaves61@gmail.com>
2023-11-08 14:45:19 -0300

    fix: add default value

--- get_next_line.h
+++ get_next_line.h
@@ -16,6 +16,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
 char	*ft_strdup(char const *s);
 char	*ft_strchr(const char *s, int c);
